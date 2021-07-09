aa9f6acd921bbb6049c5b1363dba5f54da207bb2
Hilda Ng <hildang@thoughtworks.com>
2021-07-10 05:45:00 +0800

    Refactor to extract method that does sum of number string

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,7 @@ class StringCalculator:
     def add(self, number_string):
         if not number_string:
             return 0
-        return sum(map(lambda num: int(num), number_string.split(',')))
+        return StringCalculator.sum(number_string.split(','))
+
+    def sum(numbers):
+        return sum(map(lambda num: int(num), numbers))
