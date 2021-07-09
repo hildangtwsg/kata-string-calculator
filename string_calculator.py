d97ff1629610fab67c4bd6ecfc1abbfa16a65ad8
Hilda Ng <hildang@thoughtworks.com>
2021-07-10 06:05:40 +0800

    Refactor to extract split functionality for number string

--- string_calculator.py
+++ string_calculator.py
@@ -5,7 +5,10 @@ class StringCalculator:
     def add(self, number_string):
         if not number_string:
             return 0
-        return StringCalculator.sum(re.split(',|\n', number_string))
+
+        def split_by(delimiter):
+            return re.split(delimiter, number_string)
+        return StringCalculator.sum(split_by(',|\n'))
 
     def sum(numbers):
         return sum(map(lambda num: int(num), numbers))
